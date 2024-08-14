276f81c0801e6d4e199591f736fad41283ab3cc8
ShahlaUral <suralovaehla@icloud.com>
2024-08-15 00:31:57 +0400

    StudentDashBoard

---
+++ Models/ViewModel/StudentCoursesViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace E_TutorApplicationFront.Models.ViewModel
+{
+    public class StudentCoursesViewModel
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string CourseSectionName { get; set; }
+        public string CourseImgUrl { get; set; }
+        public int CourseWatchPercentage { get; set; }
+    }
+}
